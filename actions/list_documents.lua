154d358061a96281e1b3ee594f163278c342a46b
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-11-01 00:47:22 -0400

    Query filters

--- actions/list_documents.lua
+++ actions/list_documents.lua
@@ -8,6 +8,15 @@ local uuids = {}
 
 content.walk_documents(nil, function (file_uuid, header, body)
   if header.type == model_name then
+
+    -- Filter the documents using the query params
+    for k, v in pairs(request.query) do
+      if header[k] ~= v then
+        -- Don't add this document to the list
+        return
+      end
+    end
+
     table.insert(uuids, file_uuid)
   end
 end)
