f9a07a341621ab6bf838c4ec4570ec5393abe7c4
Mitchell Tannenbaum <tannenbaum.mitchell@gmail.com>
2018-11-13 21:33:20 -0800

    Update witness.lua

--- rules/witness.lua
+++ rules/witness.lua
@@ -3,6 +3,9 @@ input_parameter: "request"
 events_table: ["witness_html"]
 
 request.method == "GET"
-and #request.path_segments == 0
-and request.query.witness
-and uuid.check(request.query.witness)
+and 
+#request.path_segments == 0
+and 
+request.query.witness
+and 
+uuid.check(request.query.witness)
