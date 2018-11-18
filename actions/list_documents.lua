91c92f739408ed3d0403be861f0a9f76c67f1ea7
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-11-18 00:37:45 -0400

    Use document.query to retrieve documents. Very close to final form

--- actions/list_documents.lua
+++ actions/list_documents.lua
@@ -6,48 +6,29 @@ input_parameters: ["request"]
 -- GET /[type]
 local model_name = request.path_segments[1]
 
-local uuids = {}
+local documents = {}
 
--- Tantivy
-log.trace("Start tantivy search")
-local uuid_field = content.schema:get_field("uuid")
-local model_field = content.schema:get_field("model")
+-- "model:" says to look specifically in the model field
+-- + means it's required
+-- model_name is a single word so there's no need to treat it specially
+local result = content.query("+model:" .. model_name)
 
-local parser = tan.query_parser_for_index(content.index, {uuid_field, model_field})
-local coll = tan.top_collector_with_limit(10)
-local result = content.index:search(parser, model_name, coll)
 for i = 1, #result do
   local doc = result[i]
-  table.insert(uuids, {
-    file = doc:get_first(uuid_field),
-    profile = "unknown_profile"
+  table.insert(documents, {
+    file = doc:get_first(content.fields.uuid),
+    profile = doc:get_first(content.fields.store)
   })
 end
-log.trace("End tantivy search")
 
---[[
-content.walk_documents(nil, function (file_uuid, header, body, profile)
+-- TODO: Use the rest of the query or update the way it works
+-- Each query parameter specifies what a document field must have
 
-  if header.model == model_name then
-
-    -- Further filter documents using the query params
-    for k, v in pairs(request.query) do
-      if header[k] ~= v then
-        -- Don't add this document to the list
-        return
-      end
-    end
-
-    table.insert(uuids, {file=file_uuid, profile=profile})
-  end
-end)
-]]
-
-if #uuids == 0 then uuids = nil end
+if #documents == 0 then documents = nil end
 
 return {
   headers = {
     ["content-type"] = "text/html",
   },
-  body = render("list_documents.html", {model=model_name, documents=uuids})
+  body = render("list_documents.html", {model=model_name, documents=documents})
 }
