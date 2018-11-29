2386a96a7e5fc015699001ade9969118e6b8b672
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-11-29 18:17:56 -0400

    document edit

---
+++ actions/request_edit.lua
@@ -0,0 +1,25 @@
+event: ["request_document_edit_form"]
+priority: 1
+input_parameters: ["request"]
+
+if request.forbidden then return end
+
+local model_name, id = request.path_segments[1], request.path_segments[2]
+local model = models[model_name]
+local fields, body, store = content.read_document(id)
+
+return {
+  headers = {
+    ["content-type"] = "text/html"
+  },
+  body = render("model_form.html", {
+    SITENAME = torchbear.settings.sitename,
+    TITLE = "Edit " .. model_name:capitalize() .. " " .. id,
+    model_name = model_name,
+    model = model,
+
+    id = id,
+    fields = fields,
+    body = body,
+  })
+}
---
+++ rules/request_edit.lua
@@ -0,0 +1,10 @@
+priority: 3
+input_parameter: "request"
+events_table: ["request_document_edit_form"]
+
+request.method == "GET"
+and #request.path_segments == 3
+and models[request.path_segments[1]]
+and uuid.check(request.path_segments[2])
+and request.path_segments[3] == "edit"
+
